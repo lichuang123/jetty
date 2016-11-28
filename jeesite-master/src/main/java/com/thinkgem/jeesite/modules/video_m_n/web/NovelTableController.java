b7b6897e570c2ef7dbfb59eb7e08ee2d3b209ac4
yanjie <1530519854@qq.com>
2016-11-28 19:57:09 +0800

    tijao

---
+++ jeesite-master/src/main/java/com/thinkgem/jeesite/modules/video_m_n/web/NovelTableController.java
@@ -0,0 +1,12 @@
+package com.thinkgem.jeesite.modules.video_m_n.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.thinkgem.jeesite.common.web.BaseController;
+
+@Controller
+@RequestMapping(value = "${adminPath}/video_m_n/noveltable")
+public class NovelTableController extends BaseController {
+
+}
